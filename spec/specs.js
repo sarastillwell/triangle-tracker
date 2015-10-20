7d5f7d91b57441761961952c44a915714516f4cf
Audrey Scrugham & Sara Stillwell <student@epicodus.com>
2015-10-19 20:55:02 -0700

    Add initial files and try to do specs

---
+++ spec/specs.js
@@ -0,0 +1,12 @@
+describe('equilateral', function() {
+  it ("is true if all sides are the same", function() {
+    expect(equilateral[2,2,2]).to.equal(true);
+
+  });
+
+  it("is false if one side is different", function() {
+    expect(equilateral[2,2,1].to.equal(false);
+
+
+  });
+});
